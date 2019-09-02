e84e6e50cf48d57d17515ce0cf0ffb08eca200ee
Jobins John <jobins666@gmail.com>
2019-09-02 10:00:41 +0400

    Update ExtensionHelpers.swift
    Added generateRandomNumbers()

--- ExtensionHelpers.swift
+++ ExtensionHelpers.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+import UIKit
+
+func generateRandomNumbers(quantity: Int) -> [CGFloat] {
+	var randomNumArray = [CGFloat]()
+	for _ in 1...quantity {
+		let randomNumber = CGFloat(arc4random_uniform(255))
+		randomNumArray.append(randomNumber)
+	}
+	return randomNumArray
+}
