f38d35f1eb840366cf838a98513b826a35ebc34e
Jobins John <jobins666@gmail.com>
2019-09-02 10:11:32 +0400

    Update UIButton+Extensions.swift

--- UIButton+Extensions.swift
+++ UIButton+Extensions.swift
@@ -95,6 +95,22 @@ extension UIButton {
 		
 	}
 	
+	//MARK: — stopLoading() => Stop loading animation in button.
+	func roundedButtonWithAvatarImage(image: UIImage, borderWidth: CGFloat = 1.0, borderColor: CGColor = UIColor.whiteColor().CGColor) {
+		self.setImage(image, forState: .Normal)
+		self.layer.cornerRadius = self.frame.size.width / 2
+		self.layer.borderWidth = borderWidth
+		self.layer.borderColor = borderColor
+		self.clipsToBounds = true
+	}
+	
+	func roundedButtonWithAvatarImageData(imageData: NSData, borderWidth: CGFloat = 1.0, borderColor: CGColor = UIColor.whiteColor().CGColor) {
+		self.roundedButtonWithAvatarImage(UIImage(data: imageData)!, borderWidth: borderWidth, borderColor: borderColor)
+	}
+	/*
+		Button.roundedButtonWithAvatarImage(UIImage(named: "image_name")!, borderWidth: 1, borderColor: UIColor.blueColor().CGColor)
+	*/
+	
 	//MARK: — HELPER FUNCTIONS SECTION
 	
 	//Helper functions for startLoading() & stopLoading()
