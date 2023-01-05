a760975d1d027bc68611706293d6b6dbb4c63a83
Jobins John <jobins.john@musashiai.com>
2023-01-05 11:09:51 -0500

    Shake feature added to UIView extension

--- UIView+Extensions.swift
+++ UIView+Extensions.swift
@@ -5,4 +5,20 @@ extension UIView {
         self.layer.shadowOffset = CGSize(width: 2, height: 4)
         self.layer.shadowRadius = 5
     }
+    
+    func shake() {
+        let shake = CABasicAnimation(keyPath: "position")
+        shake.duration = 0.05
+        shake.repeatCount = 2
+        shake.autoreverses = true
+        let fromPoint = CGPoint(x: center.x - 5, y: center.y)
+        let fromValue = NSValue(cgPoint: fromPoint)
+        let toPoint = CGPoint(x: center.x + 5, y: center.y)
+        let toValue = NSValue(cgPoint: toPoint)
+        shake.fromValue = fromValue
+        shake.toValue = toValue
+        layer.add(shake, forKey: "position")
+    }
+    //Use like this:
+    //someView.shake()
 }
