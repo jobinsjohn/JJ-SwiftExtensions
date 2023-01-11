215615beae77ca3467ee7524edca491725a0e000
Jobins John <jobins666@gmail.com>
2023-01-11 11:42:52 -0500

    Update UIView+Extensions.swift

--- UIView+Extensions.swift
+++ UIView+Extensions.swift
@@ -40,4 +40,12 @@ extension UIView {
         clipsToBounds = true
     }
     //parentView.makeRounded()
+
+    func roundCorners(corners: UIRectCorner, radius: CGFloat) {
+         let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+         let mask = CAShapeLayer()
+         mask.path = path.cgPath
+         layer.mask = mask
+     }
+    //view.roundCorners(corners: [.topLeft, .topRight], radius: 20)
 }
